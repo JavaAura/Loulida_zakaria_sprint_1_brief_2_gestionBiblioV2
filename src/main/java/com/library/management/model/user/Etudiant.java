26972d59c47482125cf41672c51e57d5b711e2f3
zakaria loulida <zakarialoulida92@gmail.com>
2024-09-12 12:14:59 +0100

    update eetudiant add to string

--- src/main/java/com/library/management/model/user/Etudiant.java
+++ src/main/java/com/library/management/model/user/Etudiant.java
@@ -1,18 +1,42 @@
 package com.library.management.model.user;
 
-public class Etudiant extends Utilisateur {
-    private String studentId;
+import java.time.LocalDate;
 
-    public Etudiant(int id, String name,String email, String studentId) {
-        super(id, name,email);
-        this.studentId = studentId;
+public class Etudiant extends Utilisateur {
+    private String niveauEtudes;
+
+    public Etudiant(String nom, String prenom, String email, String motDePasse, LocalDate dateInscription, String niveauEtudes) {
+        super(nom, prenom, email, motDePasse, dateInscription);
+        this.niveauEtudes = niveauEtudes;
     }
 
-    public String getStudentId() { return studentId; }
-    public void setStudentId(String studentId) { this.studentId = studentId; }
+    // Getters and Setters
+    public String getNiveauEtudes() {
+        return niveauEtudes;
+    }
+
+    public void setNiveauEtudes(String niveauEtudes) {
+        this.niveauEtudes = niveauEtudes;
+    }
+
 
     @Override
-    public void displayInfo() {
-       System.out.println("i'm a user");
+    public String toString() {
+        return "Etudiant { id :"+ getId() + "   name  :"+ getNom() +" " + getPrenom() +
+                "  niveauEtudes='" + niveauEtudes + "  DateInscription :  " + getDateInscription()+
+        '}';
+    }
+
+    public  void displayInfo(){
+        System.out.println("******************************************"  );
+        System.out.println("Etudiant id :"+ getId()  );
+        System.out.println("name  :"+ getNom() +"  " + getPrenom() );
+        System.out.println("Niveau d'etudes  :"+ getNiveauEtudes() + "         DateInscription :  " + getDateInscription() );
+
+    }
+
+    public  void affiche() {
+        System.out.printf(""+getEmail());
+
     }
 }
--- src/main/java/com/library/management/model/user/Professeur.java
+++ src/main/java/com/library/management/model/user/Professeur.java
@@ -1,22 +1,37 @@
 package com.library.management.model.user;
 
-public class Professeur extends Utilisateur{
-private String Departement ;
-public Professeur (int id,String name,String email ,String Departement){
-    super(id, name, email);
-    this.Departement = Departement;
+import java.time.LocalDate;
 
-}
+public class Professeur extends Utilisateur {
+    private String domaineRecherche;
 
-    public void setDepartement(String departement) {
-        Departement = departement;
+    public Professeur(String nom, String prenom, String email, String motDePasse, LocalDate dateInscription, String domaineRecherche) {
+        super(nom, prenom, email, motDePasse, dateInscription);
+        this.domaineRecherche = domaineRecherche;
     }
 
-    public String getDepartement() {
-        return Departement;
+    // Getters and Setters
+    public String getDomaineRecherche() {
+        return domaineRecherche;
+    }
+
+    public void setDomaineRecherche(String domaineRecherche) {
+        this.domaineRecherche = domaineRecherche;
+    }
+
+    @Override
+    public String toString() {
+        return "Professur{ id :"+ getId() + "  name  :"+ getNom()  + getPrenom()
+                + "  domaineRecherche='" + domaineRecherche + '\'' + "  DateInscription :  " + getDateInscription()+
+                '}';
     }
 
     public void displayInfo(){
-    System.out.println("i'm a professeur");
-}
+
+        System.out.println("******************************************************"  );
+        System.out.println("Professeur id :"+ getId()  );
+        System.out.println("name  :"+ getNom() + "       prenom : " + getPrenom() );
+        System.out.println("domaine de recherche  :"+ getDomaineRecherche() + "   DateInscription :  " + getDateInscription() );
+
+    }
 }
