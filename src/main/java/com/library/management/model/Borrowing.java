1a5c707d1c11ba68d8069f6ae24ee61ff9879a4e
zakaria loulida <zakarialoulida92@gmail.com>
2024-09-13 23:20:14 +0100

    add borrowing  model

---
+++ src/main/java/com/library/management/model/Borrowing.java
@@ -0,0 +1,53 @@
+package com.library.management.model;
+
+import java.time.LocalDate;
+
+public class Borrowing {
+    private int id;
+    private int userId;
+    private int documentId;
+
+
+    public Borrowing(int id, int userId, int documentId) {
+        this.id = id;
+        this.userId = userId;
+        this.documentId = documentId;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(int documentId) {
+        this.documentId = documentId;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Borrowing{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", documentId=" + documentId +
+
+                '}';
+    }
+}
